c337add6a7a1e4479a8881dee6713b1dda82a1bb
Guillaume SCHEIDT <guillaume.scheidt@i-datech.fr>
2025-09-03 17:24:10 +0200

    Fix unit tests for python before 3.11 and add assertions to integration tests

--- tests/test_integration.py
+++ tests/test_integration.py
@@ -22,7 +22,8 @@ async def test_lines_discovery_call(api_token):
     """lines-discovery test"""
     cts_api = CtsApi(api_token, None)
     response = await cts_api.lines_discovery()
-    assert response is not None
+    assert response.lines_delivery.annotated_line_refs is not None
+    assert response.lines_delivery.annotated_line_refs.__len__() > 0
 
 
 @pytest.mark.asyncio
@@ -30,8 +31,9 @@ async def test_lines_discovery_call(api_token):
 async def test_stoppoints_discovery_call(api_token):
     """stoppoints-discovery test"""
     cts_api = CtsApi(api_token, None)
-    response = await cts_api.stoppoints_discovery(stop_code="280a")
-    assert response is not None
+    response = await cts_api.stoppoints_discovery()
+    assert response.stop_points_delivery.annotated_stop_point_ref is not None
+    assert response.stop_points_delivery.annotated_stop_point_ref.__len__() > 0
 
 
 @pytest.mark.asyncio
@@ -39,8 +41,10 @@ async def test_stoppoints_discovery_call(api_token):
 async def test_stop_monitoring_call(api_token):
     """stop-monitoring test"""
     cts_api = CtsApi(api_token, None)
-    response = await cts_api.stop_monitoring(monitoring_ref="280a")
-    assert response is not None
+    response = await cts_api.stop_monitoring(monitoring_ref="610")
+    assert response.service_delivery.stop_monitoring_delivery is not None
+    assert response.service_delivery.stop_monitoring_delivery.__len__() > 0
+    assert response.service_delivery.stop_monitoring_delivery[0].monitored_stop_visit is not None
 
 
 @pytest.mark.asyncio
@@ -49,4 +53,4 @@ async def test_general_messages_call(api_token):
     """general-messages test"""
     cts_api = CtsApi(api_token, None)
     response = await cts_api.general_messages()
-    assert response is not None
+    assert response.service_delivery.general_message_delivery is not None
